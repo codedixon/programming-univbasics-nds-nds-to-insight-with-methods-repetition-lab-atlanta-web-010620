d5b98306c78d9b5f7fa93c6990af78075092b0b4
Rashad Dixon <dixoncodes@gmail.com>
2019-12-21 06:36:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -47,7 +47,7 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
   while index < list_of_directors(source).length
-  total += directors_totals[:name](source)
+  total += directors_totals[:name]
 end
 return total
 end
